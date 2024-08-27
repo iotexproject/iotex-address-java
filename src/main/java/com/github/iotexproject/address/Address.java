d1b05a0f85e5109837e63dc77400caae464e0fc7
ququzone <xueping.yang@gmail.com>
2024-08-27 17:41:00 +0800

    initial project

---
+++ src/main/java/com/github/iotexproject/address/Address.java
@@ -0,0 +1,26 @@
+package com.github.iotexproject.address;
+
+/**
+ * bech32.
+ *
+ * @author Yang XuePing
+ */
+public class Address {
+    public static final String AddressPrefix = "io";
+    public static String to0x(String address) {
+        if (!address.startsWith("io")) {
+            throw new RuntimeException("Error address format");
+        }
+        byte[] dec = Bech32.decode(address).data;
+        return "0x" + Numeric.toHexString(Bech32.convertBits(dec, 0, dec.length, 5, 8, false));
+    }
+
+    public static String toIo(String address) {
+        if (!address.startsWith("0x")) {
+            throw new RuntimeException("Error address format");
+        }
+        byte[] values = Numeric.hexStringToByteArray(address.substring(2));
+        byte[] grouped = Bech32.convertBits(values, 0, values.length, 8, 5, true);
+        return Bech32.encode(AddressPrefix, grouped);
+    }
+}
